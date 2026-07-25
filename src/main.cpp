e87392ac3fe20e5d04f247f1dd3c7eb73b357442
Deepak Kumar <deepaksagarsingh0@gmail.com>
2026-07-26 02:19:18 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <fstream>
 #include <string>
+#include <zlib.h>
 
 int main(int argc, char *argv[])
 {
@@ -20,7 +21,6 @@ int main(int argc, char *argv[])
     }
     
     std::string command = argv[1];
-    
     if (command == "init") {
         try {
             std::filesystem::create_directory(".git");
@@ -40,8 +40,49 @@ int main(int argc, char *argv[])
         } catch (const std::filesystem::filesystem_error& e) {
             std::cerr << e.what() << '\n';
             return EXIT_FAILURE;
-        }
-    } else {
+        } }
+    else if(command=="cat-file") {
+            std:: string hash= argv[3];
+            std::string dir= hash.substr(0,2);
+            std::string file = hash.substr(2);
+            std:: string path = ".git/objects/" + dir + "/" + file; 
+            std::ifstream objectFile(path, std::ios::binary);
+            if (!objectFile)
+            {
+                std::cerr << "Failed to open object file\n";
+                return EXIT_FAILURE;
+            }
+            std::string compressed(
+            (std::istreambuf_iterator<char>(objectFile)),
+             std::istreambuf_iterator<char>()
+             );
+             uLongf decompressedSize = compressed.size() * 20; // large enough buffer
+            std::string decompressed(decompressedSize, '\0');
+
+            int result = uncompress(
+                reinterpret_cast<Bytef*>(&decompressed[0]),
+                &decompressedSize,
+                reinterpret_cast<const Bytef*>(compressed.data()),
+                compressed.size()
+            );
+
+            if (result != Z_OK)
+            {
+                std::cerr << "Failed to decompress object\n";
+                return EXIT_FAILURE;
+            }
+
+            decompressed.resize(decompressedSize);
+            size_t pos = decompressed.find('\0');
+            if (pos == std::string::npos)
+            {
+                std::cerr << "Invalid object\n";
+                return EXIT_FAILURE;
+            }
+
+            std::cout << decompressed.substr(pos + 1);
+    }
+    else {
         std::cerr << "Unknown command " << command << '\n';
         return EXIT_FAILURE;
     }
